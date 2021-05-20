9d4179e3e21fa4e45f4268e397e223338f58aacf
2015542105 <62696347+byte7890@users.noreply.github.com>
2021-05-20 10:46:28 +0900

    Add files via upload

---
+++ 0520/nameLambda.hs
@@ -0,0 +1,12 @@
+import System.IO
+
+nameLambda :: IO ()
+nameLambda = putStr "What is your first name? " >>
+getLine >>= \ first ->
+putStr "And your last name? " >>
+getLine >>= \ last ->
+let full = first ++ " " ++ last
+in putStrLn ("Pleased to meet you, " ++ full ++ "!")
+
+main = do
+  nameLambda
